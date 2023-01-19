05e92842d112d60753661fad4c6245ef39485552
joseangelgomezbravo <joseangelgomezbravo@LEM231-14>
2023-01-18 18:56:12 -0800

    Add project files.

---
+++ COMP003A/Program.cs
@@ -0,0 +1,18 @@
+﻿//this a single-line comment
+/* this is multi-line commnent
+ * Author: Jose Gomez
+ * Course: COMP-003A
+ * Purpose: Lecture activity for this week
+ */
+namespace COMP003A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");//add comments here too
+            Console.WriteLine("this is a string");
+            Console.WriteLine("Jose Angel Gomez Bravo");
+        }
+    }
+}
