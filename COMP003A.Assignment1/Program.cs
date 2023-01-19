5294ccbda0e09e70cd5efeae9206dac84fb90903
Jose Angel Gomez Bravo <joseangelgomezbravo@my.whccd.edu>
2023-01-18 20:36:34 -0800

    intial commit

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,119 @@
+﻿//this is single line comment
+/*this is a multi-line comment
+ * 
+ * Author: Jose Angel Gomez Bravo
+ * Course: COMP-003-L01
+ * Purpose: Code for assignment 1
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 things about me");
+            Console.WriteLine("1. I am eighteen years old");
+            Console.WriteLine("2. My first year of college");
+            Console.WriteLine("3. My birthday is on Janurary 27");
+            Console.WriteLine("4. I like to go camping");
+            Console.WriteLine("5. My favorite food is pizza");
+            Console.WriteLine("6. My favorite movie is Intersteller");
+            Console.WriteLine("7. My favorite subject is math");
+            Console.WriteLine("8. My favorite sport tennis");
+            Console.WriteLine("9. The best soccer player is Messi");
+            Console.WriteLine("10. France should have won the world cup");
+            Console.WriteLine("11. My second favorite sport is soccer");
+            Console.WriteLine("12. I have 5 siblings");
+            Console.WriteLine("13. I have four brothers");
+            Console.WriteLine("14. I have one sister");
+            Console.WriteLine("15. I have a dog");
+            Console.WriteLine("16. My dog's name is Capitan");
+            Console.WriteLine("17. I'm currently working towards three majors");
+            Console.WriteLine("18. The first major is Mathematics transfers");
+            Console.WriteLine("19. The second degree is Math and Science Liberal Arts");
+            Console.WriteLine("20. The third major is phyics");
+            Console.WriteLine("21. And I'm also working towards a Computer Science certifcate");
+            Console.WriteLine("22. My brother's name is Adrian");
+            Console.WriteLine("23. My other brother's name is Rolando");
+            Console.WriteLine("24. He was named after my dad");
+            Console.WriteLine("25. My other brother's name is Pedro");
+            Console.WriteLine("26. My youngest brother's name is david");
+            Console.WriteLine("27. My sisters name is Jazmin");
+            Console.WriteLine("28. Currently I'm a student worker");
+            Console.WriteLine("29. My favorite color is blue");
+            Console.WriteLine("30. My career goal is to be a software developer");
+            Console.WriteLine("31. My favorite hobby is to go camping");
+            Console.WriteLine("32. Growing up I was a boy scout");
+            Console.WriteLine("33. I also like to play video games");
+            Console.WriteLine("34. Some of my favorite games were Halo 3");
+            Console.WriteLine("35. I also liked Subnautica");
+            Console.WriteLine("36. The game I'm currently enjoying is Apex Legends");
+            Console.WriteLine("37. My second favorite subject is history");
+            Console.WriteLine("38. The subject that I found most interesting is Chemistry");
+            Console.WriteLine("39. A hobby I find interesting is fishing");
+            Console.WriteLine("40. I like to collect hotwheels");
+            Console.WriteLine("41. I also like to cook");
+            Console.WriteLine("42. My favorite part is baking foods");
+            Console.WriteLine("43. My favorite baked dish is flan");
+            Console.WriteLine("44. My favorite Mexican dish are tamales");
+            Console.WriteLine("45. My favorite junk food is hamburgers");
+            Console.WriteLine("46. My favorite fast food place is in-n-out");
+            Console.WriteLine("47. A goal for me is to travel more");
+            Console.WriteLine("48. My favorite holiday is halloween");
+            Console.WriteLine("49. I like to spend time with my family");
+            Console.WriteLine("50. A current goal I have is doing good in my classes");
+            Console.WriteLine("51. Another goal I have is going to the gym");
+            Console.WriteLine("52. Another goal is saving up money");
+            Console.WriteLine("53. Another goal is meeting new people");
+            Console.WriteLine("54. Another goal is to decide out college I'm transfering to");
+            Console.WriteLine("55. My favorite day of the week is friday");
+            Console.WriteLine("56. My favorite music genre is rap");
+            Console.WriteLine("57. My other favorite music genre is mnexican music");
+            Console.WriteLine("58. My current car is an g35");
+            Console.WriteLine("59. My dream car is mustang gt 500");
+            Console.WriteLine("60. My favorite show is rick and morty");
+            Console.WriteLine("61. My favorite part of college is class schedule");
+            Console.WriteLine("62. For christmas I got a guitar");
+            Console.WriteLine("63. My car is white");
+            Console.WriteLine("64. My car has four wheels");
+            Console.WriteLine("65. It is also my first car");
+            Console.WriteLine("66. Which I bought by myself");
+            Console.WriteLine("67. Car insurance is very expensive");
+            Console.WriteLine("68. I wish it didn't cost so much");
+            Console.WriteLine("69. I'm also planning on buying a motorcycle");
+            Console.WriteLine("70. I feel like it would be fun");
+            Console.WriteLine("71. Because I like the freedom");
+            Console.WriteLine("72. But I don't know how to ride a motorcycle yet");
+            Console.WriteLine("73. And insurance will be expensive");
+            Console.WriteLine("74. Hopefully I can save enough money");
+            Console.WriteLine("75. Therefore I'm going to work and save money");
+            Console.WriteLine("76. Currently I don't know what type of motorcycle I want");
+            Console.WriteLine("77. All I know is that I want something fast");
+            Console.WriteLine("78. But not to fast");
+            Console.WriteLine("79. Because I might get carried away");
+            Console.WriteLine("80. And get in an accident");
+            Console.WriteLine("81. But I also don't want a slow motorcycle");
+            Console.WriteLine("82. Because I want to be able to go places outside the city");
+            Console.WriteLine("83. I also might get a newer car");
+            Console.WriteLine("84. But I don't know if I want to sell my car");
+            Console.WriteLine("85. Because its my first car");
+            Console.WriteLine("86. And I promised I would keep my first car");
+            Console.WriteLine("87. But I might break that promise");
+            Console.WriteLine("88. Because I want an upgrade");
+            Console.WriteLine("89. But I'm leaning more towards selling my car");
+            Console.WriteLine("90. Because I have a car I want to buy");
+            Console.WriteLine("91. Current car prices are high");
+            Console.WriteLine("92. So I might wait and see how prices are");
+            Console.WriteLine("93. The car I want to buy is 2013 Ford Tarus");
+            Console.WriteLine("94. Mainly because my dad had an older tarus");
+            Console.WriteLine("95. And that car was very fun to drive");
+            Console.WriteLine("96. And the newer ones look very nice");
+            Console.WriteLine("97. The newer ones are also more reliable");
+            Console.WriteLine("98. Therefore allowing me to have more fun");
+            Console.WriteLine("99. And not worry about breaking something");
+            Console.WriteLine("100. But I still don't what I will do");
+
+
+        }
+    }
+}
